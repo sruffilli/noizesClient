0f24176591a89a62865674726c029137c4450da8
Simone Ruffilli <sruffilli@sruffilli-macbookpro2.roam.corp.google.com>
2015-05-02 17:12:35 +0200

    Now supports spaces in the filename\!

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var socket = require('socket.io-client')('http://test.ruffilli.it:8888');
 socket.emit('add user');
 
 socket.on('login', function(data) {
-  console.log("Succesfully logged in as " + data.username);
+  console.log('Succesfully logged in as ' + data.username);
   console.log(data.numUsers + ' users are currently connected');
 });
 
 socket.on('play_url', function(data){
 	console.log('play_url : ' + data.url);
-	exec("mplayer -prefer-ipv4 " + data.url);
+	exec('mplayer -prefer-ipv4 "' + data.url +'"');
 });
 
