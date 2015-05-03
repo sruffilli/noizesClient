84ab083a50d564aecdbd43d19640a6a46d44bed8
Simone Ruffilli <sruffilli@sruffilli-macbookpro2.roam.corp.google.com>
2015-05-03 12:16:19 +0200

    General makeover: comments added and proper escaping of shell arguments

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-require('shelljs/global');
-var socket = require('socket.io-client')('http://localhost:8080');
+// Absolute path of the noizes service
+const serviceUrl = 'http://localhost:8888';
 
+// Used to escape shell commands
+var shellescape = require('shell-escape');
+
+// Used to invoke shell commands
+require('shelljs/global');
+
+// Create a socket client istance with serviceUrl
+var socket = require('socket.io-client')(serviceUrl);
+
+// First thing, login to the service
 socket.emit('add user');
 
 socket.on('login', function(data) {
@@ -8,12 +18,23 @@ socket.on('login', function(data) {
   console.log(data.numUsers + ' users are currently connected');
 });
 
-socket.on('play_url', function(data){
-	console.log('play_url : ' + data.url);
-	exec('mplayer -prefer-ipv4 "' + data.url +'"', {async:true});
+// Invoke mplayer when receiving the play_url command
+socket.on('play_url', function(data) {
+  console.log('play_url : ' + data.url);
+
+  var mplayerCMD = ['mplayer', '-prefer-ipv4', data.url];
+
+  console.log('Running command ' + shellescape(mplayerCMD))
+
+  exec(shellescape(mplayerCMD) + '>/dev/null 2>&1', {
+    async: true
+  });
 });
 
-socket.on('stop', function(data){
-	console.log('stop');
-	exec('killall mplayer');
+// Killall mplayer istances (YOLO) when receiving the stop command
+socket.on('stop', function(data) {
+  var killallCMD = 'killall mplayer';
+  console.log('stop');
+  console.log('Running command ' + killallCMD)
+  exec(killallCMD);
 });
